c04d10125d583b168d6e922282da81a9523e7e51
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-12-11 14:33:39 -0300

    feat: modelando o banco de dados e fazendo a migração

--- apps/administrators/models.py
+++ apps/administrators/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import Permission
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    Group,
+    BaseUserManager,
+)
+import uuid
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("O usuário deve ter um email")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    adminstrator_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    email = models.EmailField(unique=True, verbose_name="Email")
+    name = models.CharField(max_length=100, verbose_name="Nome")
+    password = models.CharField(max_length=128, verbose_name="Senha")
+    document = models.CharField(max_length=15, verbose_name="CPF")
+    registration_code = models.CharField(
+        max_length=45, unique=True, verbose_name="Código de Registro"
+    )
+    photo = models.ImageField(
+        upload_to="admin_photos/", blank=True, verbose_name="Foto"
+    )
+    key = models.CharField(max_length=100, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Atualizado em")
+
+    user_permissions = models.ManyToManyField(Permission, blank=True)
+    groups = models.ManyToManyField(Group, related_name="administrators", blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.photo and not self.key:
+            self.key = validate_image_key(self.photo.name)
+        super().save(*args, **kwargs)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+import re
+import uuid
+from apps.programs.models import Program
 
-# Create your models here.
+
+def validate_image_key(image_path):
+    match = re.search(r"([^/]+)(?=\.[^.]+$)", image_path)
+    return match.group(0) if match else None
+
+
+def validate_image_size(image):
+    MAX_SIZE = 5 * 1024 * 1024
+    if image.size > MAX_SIZE:
+        raise ValidationError("A imagem deve ter no máximo 5 MB.")
+
+
+def validate_cpf(cpf):
+
+    cpf = re.sub(r"[^0-9]", "", cpf)
+
+    if len(cpf) != 11:
+        raise ValidationError("CPF deve ter 11 dígitos.")
+
+    if cpf == cpf[0] * len(cpf):
+        raise ValidationError("CPF inválido.")
+
+    def calcular_dv(cpf, peso):
+        soma = sum(int(digito) * peso for digito, peso in zip(cpf, peso))
+        resto = soma % 11
+        if resto < 2:
+            return 0
+        return 11 - resto
+
+    peso1 = list(range(10, 1, -1))
+    dv1 = calcular_dv(cpf[:9], peso1)
+
+    peso2 = list(range(11, 1, -1))
+    dv2 = calcular_dv(cpf[:10], peso2)
+
+    if int(cpf[9]) != dv1 or int(cpf[10]) != dv2:
+        raise ValidationError("CPF inválido.")
+
+    return cpf
+
+
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=100, verbose_name="Nome")
+    document = models.CharField(
+        unique=True,
+        max_length=15,
+        verbose_name="CPF",
+        validators=[validate_cpf],
+    )
+
+    course = models.CharField(max_length=45, verbose_name="Curso")
+    registration_code = models.CharField(
+        unique=True, max_length=45, verbose_name="Código de Matrícula"
+    )
+    photo = models.ImageField(
+        upload_to="user_photos/", validators=[validate_image_size], verbose_name="Foto"
+    )
+    key = models.CharField(
+        unique=True, max_length=100, blank=True, verbose_name="Chave da Imagem"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Atualizado em")
+    program = models.ForeignKey(
+        Program, on_delete=models.SET_NULL, null=True, related_name="users"
+    )
+
+    def save(self, *args, **kwargs):
+        if self.photo and not self.key:
+            self.key = validate_image_key(self.photo.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username

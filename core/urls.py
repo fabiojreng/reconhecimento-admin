81fd3cab7a1d5a4a44c762dac38a00abcfbf5aee
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-12-12 11:20:56 -0300

    feat: adicionar urls dos programas

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.users.urls")),
+    path("", include("apps.programs.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

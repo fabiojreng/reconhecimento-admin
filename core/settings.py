e96cb7bfb4ef4d25d8e1e75e5492fd463ca79ac3
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-12-12 09:28:25 -0300

    feat: criar template base, paginação e configurar

--- core/settings.py
+++ core/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["core/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -113,4 +113,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-AUTH_USER_MODEL = "administrators.User"
+AUTH_USER_MODEL = "administrators.Administrator"

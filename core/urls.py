ed88f3f1e900500a0a1af2c75acd4b52ae318fc0
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-12-15 18:26:35 -0300

    feat: adicionar rotas de administrators

--- core/urls.py
+++ core/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", include("apps.users.urls")),
     path("", include("apps.programs.urls")),
+    path("", include("apps.administrators.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
